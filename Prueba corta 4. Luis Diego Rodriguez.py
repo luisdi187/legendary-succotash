7c005d154cc42f9f30fedb60c2ddc872a501b376
luisdi187 <105998215+luisdi187@users.noreply.github.com>
2022-05-21 18:40:03 -0600

    Add files via upload

---
+++ Prueba corta 4. Luis Diego Rodriguez.py
@@ -0,0 +1,12 @@
+def operacionesEnSublistas(pLista1, pLista2):
+        while pLista1 != []:
+            numero1 = pLista1[0]
+            pLista1 = pLista1[1:]
+            operacion = pLista1[0]
+            pLista1 = pLista1[1:]
+            numero2 = pLista1[0]
+            pLista1 = pLista1[1:]
+        if operacion == "+":
+            resultado = numero1 + numero2
+            print (resultado)
+operacionesEnSublistas([[51,"+",2],[8,"-", 25]], [2,14])
